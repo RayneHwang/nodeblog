9ca9bedfe4ea9617e2833697a84e37e92828355a
Lei,HUANG <mrsatangel@gmail.com>
2016-08-09 08:46:35 -0400

    fix

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,24 +1,33 @@
 if (window.location.pathname == '/') {
 	(function () {
+		var flag=0;
+		var posted=0;
 		$('#addTopicBtn').click(function () {
 			$.ajax({
 				'type': 'GET',
 				//TODO Debugin on Chrome requires the IP of host
-				'url': 'http://192.168.1.105:3000/users/isLoggedIn',
+				'url': '/users/isLoggedIn',
 				'dataType': 'json',
 				'async': false,
 				'success': function (data) {
 					if (data.status == 0) {
+						flag=1;
 						alert('此操作需要登陆！');
 						location.assign('/user/login');
 					}
-					else
+					else{
+						flag=1;
 						return;
+					}
 				},
 				'error': function () {
 					alert('Failed!');
 				}
-			})
-		})
+			});
+			posted=1;
+			while(flag == 0 && posted == 1){};
+		});
+
+
 	})();
 }
