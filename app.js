09332308c9012bf3a02cbbcd5631ad382b34f31c
Lei,HUANG <mrsatangel@gmail.com>
2016-11-26 10:47:39 +0800

    reformat some code

--- app.js
+++ app.js
@@ -40,32 +40,32 @@ global.appRoot = path.resolve(__dirname);
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 app.use(session({
-	secret: config.session.cookie.secret,
-	name: config.session.cookie.name,
-	cookie: {
-		maxAge: config.session.cookie.maxAge
-	},
-	resave: false,
-	saveUninitialized: true,
-	store: new MongoStore({url: config.session.database.address})
+  secret: config.session.cookie.secret,
+  name: config.session.cookie.name,
+  cookie: {
+    maxAge: config.session.cookie.maxAge
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({url: config.session.database.address})
 }));
 
 var csrfProtection = csrf();
 app.use(csrfProtection);
 
 app.use(function (err, req, res, next) {
-	if (err.code !== 'EBADCSRFTOKEN') return next(err)
-	// handle CSRF token errors here
-	var name = 'submit';
-	var notice = 'Invalid csrf token , please refresh';
-	helps.resJsonError(req, res, name, notice);
+  if (err.code !== 'EBADCSRFTOKEN') return next(err)
+  // handle CSRF token errors here
+  var name = 'submit';
+  var notice = 'Invalid csrf token , please refresh';
+  helps.resJsonError(req, res, name, notice);
 })
 
 //config the web page info
 app.use(function (req, res, next) {
-	res.locals.session = req.session;
-	res.locals.webName = config.web.name;
-	next();
+  res.locals.session = req.session;
+  res.locals.webName = config.web.name;
+  next();
 });
 
 app.use('/', routes);
@@ -75,32 +75,33 @@ app.use('/upload', upload);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 // error handlersds
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function (err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
+
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
