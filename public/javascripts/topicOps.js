6966ef6ffab5b7a5e909e21ca3de95f3a83d8e6a
Lei,HUANG <mrsatangel@gmail.com>
2017-03-25 09:58:51 +0800

    update delete topic interaction

--- public/javascripts/topicOps.js
+++ public/javascripts/topicOps.js
@@ -2,13 +2,19 @@
  * Created by lei on 17-3-24.
  */
 
+var topicIdToDelete = null;
 
-var deleteTopic = (function ($) {
-  return function deleteTopic(id) {
-    $('#myModal').modal("show");
-    // $.post("/topic/delete", {topicId: id}, function (data, status) {
-    //   var selector="a[href='/topic/show/?_id=" + id + "']";
-    //   $(selector).parent().remove();
-    // });
-  }
-})($);
+function openDeleteConfirmDialog(id) {
+  $('#myModal').modal("show");
+  topicIdToDelete = id;
+}
+
+function deleteTopic() {
+  if (!topicIdToDelete) return;
+  
+  $.post("/topic/delete", {topicId: topicIdToDelete}, function (data, status) {
+    var selector = "a[href='/topic/show/?_id=" + topicIdToDelete + "']";
+    $(selector).parent().remove();
+    topicIdToDelete = null;
+  });
+}
